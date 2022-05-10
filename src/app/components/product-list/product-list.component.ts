4335915ff4088e4f4841c3efaa8fbb67cc0daee8
Ucup Topekox <ucup.topekox@gmail.com>
2022-05-11 00:40:44 +0800

    create cart service

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -26,6 +28,7 @@ export class ProductListComponent implements OnInit {
   previousKeyword!: string;
 
   constructor(private productService: ProductService,
+    private cartService: CartService,    
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -134,6 +137,15 @@ export class ProductListComponent implements OnInit {
     this.thePageSize = pageSize;
     this.thePageNumber = 1;
     this.listProduct();
+  }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart : ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    // call service add cart
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 
 }
