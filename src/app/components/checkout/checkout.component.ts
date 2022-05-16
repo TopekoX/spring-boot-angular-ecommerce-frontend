2e2fc8a023d12301d487d385eb77f4aa8cb647fe
Ucup TopekoX <ucup.topekox@gmail.com>
2022-05-16 21:01:47 +0800

    add fiture creditcard when selected diferent year setup month

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -87,4 +87,26 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
+  handleMonthAndYear() {
+    const creditCardFormGroup = this.chekoutFormGroup.get('creditCard');
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.shopFormService.getCreditCardMonth(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card month: " + JSON.stringify(data));
+        this.creditCardMonth = data;
+      }
+    );
+  }
+
 }
