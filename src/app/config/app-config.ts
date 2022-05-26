cff03dc07cce4beecf0c5f955c9d513ad9bc13b1
Ucup TopekoX <ucup.topekox@gmail.com>
2022-05-27 01:50:18 +0800

    Add HTTPS

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa54cx7178kVTLRg5d7',
         issuer: 'https://dev-56796197.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
